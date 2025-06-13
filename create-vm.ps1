56c138e9694f68b35abb53648373ed497c6d9d4c
Ubuntu <azueruser@vmproj7.suca2hpt2qpubocm53p2zic25c.bx.internal.cloudapp.net>
2025-06-13 18:01:36 +0000

    Updated script with hardcoded credentials

--- create-vm.ps1
+++ create-vm.ps1
@@ -1,21 +1,22 @@
-# Define variables
 $resourceGroup = "MyResourceGroup"
-$location = "EastUS"
-$vmName = $resourceGroup = "MyResourceGroup"
 $location = "EastUS"
 $vmName = "MyVM"
 
-# Suppress prompt if the resource group already exists
+# Create resource group
 New-AzResourceGroup -Name $resourceGroup -Location $location -Force
 
-$cred = Get-Credential
+# Create credential object directly
+$username = "azureuser"
+$password = ConvertTo-SecureString "Deepanshu1@2" -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential ($username, $password)
 
-New-AzVM -ResourceGroupName $resourceGroup `
-         -Name $vmName `
-         -Location $location `
-         -VirtualNetworkName "$vmName-Vnet" `
-         -SubnetName "$vmName-Subnet" `
-         -SecurityGroupName "$vmName-nsg" `
-         -PublicIpAddressName "$vmName-ip" `
-         -Credential $cred
-
+# Create VM
+New-AzVM `
+    -ResourceGroupName $resourceGroup `
+    -Name $vmName `
+    -Location $location `
+    -VirtualNetworkName "$vmName-Vnet" `
+    -SubnetName "$vmName-Subnet" `
+    -SecurityGroupName "$vmName-nsg" `
+    -PublicIpAddressName "$vmName-ip" `
+    -Credential $cred
